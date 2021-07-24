3b7032058a8b432a7bfedbd2ed5bfbc7ea28ea59
Edilson Alves <email@edilsonalves.com>
2021-07-24 14:09:05 -0300

    fix: typeorm settings

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,15 +1,17 @@
-export default {
-  name: 'default',
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  synchronize: false,
-  entities: [process.env.ENTITES_PATH],
-  migrations: [process.env.MIGRATIONS_PATH],
-  cli: {
-    migrationsDir: process.env.MIGRATIONS_ROOT
+export default [
+  {
+    name: 'default',
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    synchronize: false,
+    entities: [process.env.ENTITES_PATH],
+    migrations: [process.env.MIGRATIONS_PATH],
+    cli: {
+      migrationsDir: process.env.MIGRATIONS_ROOT
+    }
   }
-}
+]
